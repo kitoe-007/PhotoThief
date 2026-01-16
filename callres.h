f97f41cd4367e7fc84adcf3ddcc02dc5218b7aaa
mikonova <mikk4business@gmail.com>
2026-01-17 02:04:16 +0300

    added error-fetching include file. Removed testing files .
    Client and server will be rewritten

---
+++ callres.h
@@ -0,0 +1,29 @@
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+#include <stdio.h>
+#include <windows.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <stdbool.h>
+#ifdef CALLWRAP
+
+// check the result integer for a potential errors, retrieve last error
+void ResultWrap(int result) {
+  if (result != 0) {
+    printf("Error occured upon calling: %d, error code in result: %d\n", WSAGetLastError(), result);
+    WSACleanup();
+  }
+  else if (result == SOCKET_ERROR) {
+    printf("Socket failed with an error: %d\n", WSAGetLastError());
+  }
+}
+// check the socket for potential errors
+void CheskSock(SOCKET s){
+  if (s == INVALID_SOCKET){
+    printf("something's wrong with the socket settings, stopping now (%d)\n", WSAGetLastError());
+    closesocket(s);
+    WSACleanup();
+    }
+}
+#endif
