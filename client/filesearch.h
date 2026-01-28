b778182d052751929247b9762f94eccf381e4838
mikonova <mikk4business@gmail.com>
2026-01-28 15:01:53 +0300

    добавлено нихуя

--- client/filesearch.h
+++ client/filesearch.h
@@ -2,6 +2,7 @@
 #include <windows.h>
 #include <wchar.h>
 #include <sec_api/string_s.h>
+#include <sec_api/stdlib_s.h>
 #include <stdbool.h>
 
 #define DEF_LIST_SIZE 1
@@ -14,22 +15,24 @@ typedef struct PictureInfo {
 
 
 // Get the name of the picture, pass to a struct
-static inline void GetPicName(PICTURE_INFO *PictureInfo) {
+static inline void GetPicName(PICTURE_INFO **PictureInfo) {
 	int name_idx = 0;
-	int path_idx = wcslen(PictureInfo->pic_full_path)-1;
-	while (PictureInfo->pic_full_path[path_idx] != L'\\') {
+	int path_idx = wcslen((*PictureInfo)->pic_full_path)-1;
+	while ((*PictureInfo)->pic_full_path[path_idx] != L'\\') {
 		path_idx--;
 	}
-	while (path_idx > wcslen(PictureInfo->pic_full_path)-1 && PictureInfo->pic_full_path[path_idx] != '\0') {
-		path_idx++;
-		PictureInfo->pic_name[name_idx] = PictureInfo->pic_full_path[path_idx];
+	path_idx++;
+	while (path_idx > wcslen((*PictureInfo)->pic_full_path)-1 && (*PictureInfo)->pic_full_path[path_idx] != '\0') {
+		
+		(*PictureInfo)->pic_name[name_idx] = (*PictureInfo)->pic_full_path[path_idx];
 		name_idx++;
+		path_idx++;
 	}
-	PictureInfo->pic_name[name_idx] = '\0';
+	(*PictureInfo)->pic_name[name_idx] = '\0';
 }
 
 
-static inline void DeterminePicList(PICTURE_INFO **pic_list ,wchar_t full_path[255]) {
+static inline void DeterminePicList(PICTURE_INFO **pic_list ,wchar_t *full_path) {
 	if (*pic_list == NULL) {
 		*pic_list = (PICTURE_INFO*)calloc(DEF_LIST_SIZE, sizeof(PICTURE_INFO));
 		size_increment = 1;
@@ -37,7 +40,7 @@ static inline void DeterminePicList(PICTURE_INFO **pic_list ,wchar_t full_path[2
 	}
 	else {
 		size_increment++;
-		*pic_list = (PICTURE_INFO*)realloc(*pic_list, sizeof(PICTURE_INFO)* size_increment);
+		*pic_list = (PICTURE_INFO*)realloc(*pic_list, sizeof(PICTURE_INFO) * size_increment );
 		wcscpy_s((*pic_list)->pic_full_path, sizeof((*pic_list)->pic_full_path)/sizeof(wchar_t), full_path);
 	}
 }
@@ -45,4 +48,15 @@ static inline void DeterminePicList(PICTURE_INFO **pic_list ,wchar_t full_path[2
 static inline void DestroyPicList(PICTURE_INFO **pic_list) {
 	free(*pic_list);
 	size_increment = 0;
+}
+
+static inline size_t ToMultiByte(char **dest, wchar_t *source) {
+	size_t str_size;
+	wcstombs_s(&str_size, *dest, sizeof(*dest), source, sizeof(source));
+	return str_size;
+}
+static inline size_t ToWideChar(wchar_t **dest, char *source) {
+	size_t str_size;
+	mbstowcs_s(&str_size, *dest, sizeof(*dest),source, sizeof(source));
+	return str_size;
 }
